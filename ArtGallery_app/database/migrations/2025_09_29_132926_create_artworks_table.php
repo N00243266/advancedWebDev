868d355560680cd2bfa590fd444c49be97ef1e17
Diana <N00243266@iadt.ie>
2025-09-30 13:49:35 +0100

    commit

---
+++ ArtGallery_app/database/migrations/2025_09_29_132926_create_artworks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('image');
+            $table->text('description')->nullable();
+            $table->string('artist');
+            $table->date('year');
+            $table->string('price');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artworks');
+    }
+};
---
+++ ArtGallery_app/database/seeders/ArtworkSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Models\Artwork;
+
+class ArtworkSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $currentTimestamp = Carbon::now();
+
+        Artwork::insert([
+            [
+                'title' => 'Starry Night',
+                'genre' => 'Post-Impressionism',
+                'image' => 'starry_night.jpg',
+                'description' => 'A famous painting by Vincent van Gogh.',
+                'artist' => 'Vincent van Gogh',
+                'year' => '1889-06-01',
+                'price' => '$100 million',
+                'comments' => 'One of the most recognized pieces of art in the world.',
+            ],
+            [
+                'title' => 'Mona Lisa',
+                'genre' => 'Renaissance',
+                'image' => 'mona_lisa.jpg',
+                'description' => 'A portrait painting by Leonardo da Vinci.',
+                'artist' => 'Leonardo da Vinci',
+                'year' => '1503-10-01',
+                'price' => '$860 million',
+                'comments' => 'Famous for her enigmatic expression.',
+            ],
+            [
+                'title' => 'The Persistence of Memory',
+                'genre' => 'Surrealism',
+                'image' => 'persistence_of_memory.jpg',
+                'description' => 'A surreal painting by Salvador Dalí.',
+                'artist' => 'Salvador Dalí',
+                'year' => '1931-04-01',
+                'price' => '$150 million',
+                'comments' => "Known for its melting clocks.",
+            ],
+        ]);
+
+    }
+}
